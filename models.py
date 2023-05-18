928e4bb565b529a47f2fe0c980989a8369bf7dee
Amelia Jochna <amei70bieber@gmail.com>
2023-05-18 12:26:49 +0200

    tmp

--- models.py
+++ models.py
@@ -1,5 +1,3 @@
-import datetime as _dt
-
 import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
 import passlib.hash as _hash
@@ -8,25 +6,24 @@ import database as _database
 
 
 class User(_database.Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
     id = _sql.Column(_sql.Integer, primary_key=True, index=True)
     email = _sql.Column(_sql.String, unique=True, index=True)
     hashed_password = _sql.Column(_sql.String)
 
-    tasks = _orm.relationship("Task", back_populates="owner")
+    tasks = _orm.relationship('Task', back_populates='owner')
 
     def verify_password(self, password: str):
         return _hash.bcrypt.verify(password, self.hashed_password)
 
 
 class Task(_database.Base):
-    __tablename__ = "tasks"
+    __tablename__ = 'tasks'
     id = _sql.Column(_sql.Integer, primary_key=True, index=True)
-    owner_id = _sql.Column(_sql.Integer, _sql.ForeignKey("users.id"))
+    owner_id = _sql.Column(_sql.Integer, _sql.ForeignKey('users.id'))
     state = _sql.Column(_sql.String, index=True)
     title = _sql.Column(_sql.String, index=True)
     description = _sql.Column(_sql.String, index=True)
-    priority = _sql.Column(_sql.String, index=True, default="")
-   
+    priority = _sql.Column(_sql.String, index=True, default='')
 
-    owner = _orm.relationship("User", back_populates="tasks")
+    owner = _orm.relationship('User', back_populates='tasks')
--- services.py
+++ services.py
@@ -6,9 +6,10 @@ import passlib.hash as _hash
 
 import database as _database, models as _models, schemas as _schemas
 
-oauth2schema = _security.OAuth2PasswordBearer(tokenUrl="/api/token")
+oauth2schema = _security.OAuth2PasswordBearer(tokenUrl='/api/token')
 
-JWT_SECRET = "myjwtsecret"
+JWT_SECRET = 'myjwtsecret'
+
 
 def create_database():
     return _database.Base.metadata.create_all(bind=_database.engine)
@@ -21,44 +22,48 @@ def get_db():
     finally:
         db.close()
 
+
 async def get_user_by_email(email: str, db: _orm.Session):
     return db.query(_models.User).filter(_models.User.email == email).first()
 
+
 async def create_user(user: _schemas.UserCreate, db: _orm.Session):
-    user_obj = _models.User(
-        email=user.email, hashed_password=_hash.bcrypt.hash(user.hashed_password)
-    )
+    user_obj = _models.User(email=user.email, hashed_password=_hash.bcrypt.hash(user.hashed_password))
     db.add(user_obj)
     db.commit()
     db.refresh(user_obj)
     return user_obj
 
+
 async def authenticate_user(email: str, password: str, db: _orm.Session):
     user = await get_user_by_email(db=db, email=email)
-    
+
     if not user:
         return False
 
     if not user.verify_password(password):
-        return False 
+        return False
 
     return user
+
 
 async def create_token(user: _models.User):
     user_obj = _schemas.User.from_orm(user)
 
     token = _jwt.encode(user_obj.dict(), JWT_SECRET)
 
-    return dict(access_token = token, token_type = "bearer")
+    return dict(access_token=token, token_type='bearer')
 
-async def get_current_user( db: _orm.Session = _fastapi.Depends(get_db), token: str = _fastapi.Depends(oauth2schema)):
+
+async def get_current_user(db: _orm.Session = _fastapi.Depends(get_db), token: str = _fastapi.Depends(oauth2schema)):
     try:
-        payload = _jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
-        user = db.query(_models.User).get(payload["id"])
+        payload = _jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        user = db.query(_models.User).get(payload['id'])
     except:
-        raise _fastapi.HTTPException(status_code=401, detail="Invalid email or Password")
-    
+        raise _fastapi.HTTPException(status_code=401, detail='Invalid email or Password')
+
     return _schemas.User.from_orm(user)
+
 
 async def create_task(user: _schemas.User, db: _orm.Session, task: _schemas.TaskCreate):
     task = _models.Task(**task.dict(), owner_id=user.id)
@@ -67,35 +72,40 @@ async def create_task(user: _schemas.User, db: _orm.Session, task: _schemas.Task
     db.refresh(task)
     return _schemas.Task.from_orm(task)
 
+
 async def get_tasks(user: _schemas.User, db: _orm.Session):
     tasks = db.query(_models.Task).filter_by(owner_id=user.id)
 
     return list(map(_schemas.Task.from_orm, tasks))
 
+
 async def _task_selector(task_id: int, user: _schemas.User, db: _orm.Session):
     task = db.query(_models.Task).filter_by(owner_id=user.id).filter(_models.Task.id == task_id).first()
 
     if task is None:
-        raise _fastapi.HTTPException(status_code=404, detail="Task does not exist")
-    
+        raise _fastapi.HTTPException(status_code=404, detail='Task does not exist')
+
     return task
 
+
 async def get_task(task_id: int, user: _schemas.User, db: _orm.Session):
-    task=await _task_selector(task_id=task_id, user=user, db=db)
+    task = await _task_selector(task_id=task_id, user=user, db=db)
 
     return _schemas.Task.from_orm(task)
+
 
 async def delete_task(task_id: int, user: _schemas.User, db: _orm.Session):
     task = await _task_selector(task_id, user, db)
     db.delete(task)
     db.commit()
 
-async def update_task(task_id: int, task:_schemas.TaskCreate, user: _schemas.User, db: _orm.Session):
+
+async def update_task(task_id: int, task: _schemas.TaskCreate, user: _schemas.User, db: _orm.Session):
     task_db = await _task_selector(task_id, user, db)
-    task_db.state=task.state
-    task_db.title=task.title
-    task_db.description=task.description
-    task_db.priority=task.priority
+    task_db.state = task.state
+    task_db.title = task.title
+    task_db.description = task.description
+    task_db.priority = task.priority
 
     db.commit()
     db.refresh(task_db)
